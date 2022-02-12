53b8112434a6b94a2c3a0fe869ccb802e09fb934
Jaydeep Yadav <yadavjay374@gmail.com>
2022-02-13 00:03:50 +0530

    final commit

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Getting Started with Create React App
 
+codemirror and react-codemirror2
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
---
+++ src/components/Code.jsx
@@ -0,0 +1,48 @@
+import { Box } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import { useContext } from "react";
+import { DataContext } from "../context/DataProvider";
+//? Components
+import Editor from "./Editor";
+
+const Container = styled(Box)`
+  background-color: #060606;
+  height: 50vh;
+  display: flex;
+`;
+
+const Code = () => {
+  const { html, setHtml, css, setCss, js, setJs } = useContext(DataContext);
+  return (
+    <>
+      <Container>
+        <Editor
+          heading="HTML"
+          icon="/"
+          color="#FF3C41"
+          language='xml'
+          value={html}
+          onChange={setHtml}
+        />
+        <Editor
+          heading="CSS"
+          icon="*"
+          color="#0EBEFF"
+          language='css'
+          value={css}
+          onChange={setCss}
+        />
+        <Editor
+          heading="JS"
+          icon="()"
+          color="#FCD000"
+          language='javascript'
+          value={js}
+          onChange={setJs}
+        />
+      </Container>
+    </>
+  );
+};
+
+export default Code;
